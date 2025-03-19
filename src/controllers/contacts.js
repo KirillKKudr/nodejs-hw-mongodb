8a21091176fed8554859ec9a22fe7f37c99d7a77
KirillKKudr <kirillboss2109@gmail.com>
2025-03-19 19:03:35 +0200

    done

---
+++ src/controllers/contacts.js
@@ -0,0 +1,34 @@
+import { getAllContacts, getContactById } from "../services/contactsService.js";
+import createError from "http-errors";
+
+export const getContacts = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      throw createError(404, "Contact not found");
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
