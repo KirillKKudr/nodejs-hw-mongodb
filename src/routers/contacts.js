ef5399a29283e81ca839033950024e96753bdb96
KirillKKudr <kirillboss2109@gmail.com>
2025-03-27 19:24:13 +0200

    first try

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { getContacts, getContact, addContact, updateContact, deleteContact } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { isValidId } from "../middlewares/isValidId.js";
+import { contactSchema } from "../validations/contactValidation.js";
 
 const router = express.Router();
 
 router.get("/", ctrlWrapper(getContacts));
-router.get("/:contactId", ctrlWrapper(getContact));
-router.post("/", ctrlWrapper(addContact));  
-router.patch("/:contactId", ctrlWrapper(updateContact)); 
-router.delete("/:contactId", ctrlWrapper(deleteContact)); 
+router.get("/:contactId", isValidId, ctrlWrapper(getContact));
+router.post("/", validateBody(contactSchema), ctrlWrapper(addContact));
+router.patch("/:contactId", isValidId, validateBody(contactSchema), ctrlWrapper(updateContact));
+router.delete("/:contactId", isValidId, ctrlWrapper(deleteContact));
 
 export default router;
